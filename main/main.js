301ea80069e0077e53e57aea8338724f8144c78c
Franeksztylka <franeksztylka99@gmail.com>
2026-05-25 21:01:56 +0200

    almost done a hero section

--- main/main.js
+++ main/main.js
@@ -1,8 +1,17 @@
-const btn = document.querySelector(".hamburger")
-const navUl = document.getElementById("navbar-links")
+const btn = document.querySelector('.hamburger')
+const navUl = document.getElementById('navbar-links')
 
 btn.addEventListener('click', () => {
-  btn.classList.toggle('open');
-  navUl.classList.toggle('navbar-open')
-});
-  
+	btn.classList.toggle('open')
+	navUl.classList.toggle('navbar-open')
+})
+
+const bg = document.getElementById('hero__bg')
+
+const numberOfColorBoxes = 49;
+
+for (let i = 0; i < numberOfColorBoxes; i++) {
+	const colorBox = document.createElement('div')
+	colorBox.classList.add('colorBox')
+	bg.append(colorBox)
+}
