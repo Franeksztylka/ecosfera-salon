91bb3870d99d2779cfc789b75e676f326312e2a6
HubcioBubcio123 <2pgr1@NUKLEONIK.LOCAL>
2026-05-25 14:03:07 +0200

    adding a working burger menu

--- main/main.js
+++ main/main.js
@@ -1,5 +1,8 @@
 const btn = document.querySelector(".hamburger")
+const navUl = document.getElementById("navbar-links")
 
 btn.addEventListener('click', () => {
   btn.classList.toggle('open');
+  navUl.classList.toggle('navbar-open')
 });
+  
