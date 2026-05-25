8f30e62d54278fbe74da5439f361d9881a6d63b3
HubcioBubcio123 <hubonbubon@gmail.com>
2026-05-26 01:46:38 +0200

    repairing leaf animation on hero

--- main/main.js
+++ main/main.js
@@ -1,43 +1,56 @@
-const navBar = document.querySelector('.navbar')
-const btn = document.querySelector('.hamburger')
-const navUl = document.getElementById('navbar-links')
-const navLinks = document.querySelectorAll('.navbar-links-menu')
-const burgerSpan = document.querySelectorAll('.span-burger')
-const bg = document.getElementById('hero__bg')
-const numberOfColorBoxes = 49;
+	document.addEventListener('DOMContentLoaded', () => { //ŁADOWANIE DOM ,GWARANTUJE STABILNOSC DZIALANIA
+    
+	
+	const navBar = document.querySelector('.navbar')
+	const btn = document.querySelector('.hamburger')
+	const navUl = document.getElementById('navbar-links')
+	const navLinks = document.querySelectorAll('.navbar-links-menu')
+	const burgerSpan = document.querySelectorAll('.span-burger')
+	const bg = document.getElementById('hero__bg')
+	const numberOfColorBoxes = 49;
 
 
-btn.addEventListener('click', () => {
-	btn.classList.toggle('open')
-	navUl.classList.toggle('navbar-open')
-	navBar.classList.toggle('navbar-open-bg')
-	burgerSpan.forEach(function(span){
-		span.classList.toggle('cross-color')
+	btn.addEventListener('click', () => {
+		btn.classList.toggle('open')
+		navUl.classList.toggle('navbar-open')
+		navBar.classList.toggle('navbar-open-bg')
+		if(navBar.classList.contains('navbar-open-bg')){
+			burgerSpan.forEach(function(span){
+				span.classList.add('cross-color')
+			})
+		}else{
+			burgerSpan.forEach(function(span){
+				span.classList.remove('cross-color')
+			})
+		}
 	})
-})
 
-for (let i = 0; i < numberOfColorBoxes; i++) {
-	const colorBox = document.createElement('div')
-	colorBox.classList.add('colorBox')
-	bg.append(colorBox)
-}
-
-navLinks.forEach(function(link){
-
-	link.addEventListener('click', () => {
-		navUl.classList.remove('navbar-open')
-		btn.classList.remove('open')
+	navLinks.forEach(function(link){
+		link.addEventListener('click', () => {
+			navUl.classList.remove('navbar-open')
+			burgerSpan.forEach(function(span){
+				span.classList.remove('cross-color')
+			})
+			btn.classList.remove('open')
+			navBar.classList.remove('navbar-open-bg')
+		})
 	})
-})
 
-window.addEventListener('scroll' ,() =>{
-
-	if(window.scrollY > 50){
-		navBar.classList.add('scroll-bg')
-	}else{
-		navBar.classList.remove('scroll-bg')
+	for (let i = 0; i < numberOfColorBoxes; i++) {
+		const colorBox = document.createElement('div')
+		colorBox.classList.add('colorBox')
+		bg.append(colorBox)
 	}
 
-})
 
+	window.addEventListener('scroll' ,() =>{
 
+		if(window.scrollY > 50){
+			navBar.classList.add('scroll-bg')
+		}else{
+			navBar.classList.remove('scroll-bg')
+		}
+
+	})
+
+});
